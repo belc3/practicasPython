f5074d9ec94b7396caa0d8721cad6bd48d13a626
belc3 <isc.davidmontero@gmail.com>
2023-04-04 23:51:26 -0600

    Subo primer ejercicio [Concatenaciones para un nombre

---
+++ Concatenacion.py
@@ -0,0 +1,4 @@
+color = input("¿Cual es el color de tu cerveza favorita?")
+banda = input("¿Cual es tu banda favorita?")
+print("Felicidades, el mejor nombre para tu cerveza es: \nLa \n" + color+ " \n" + banda)
+
